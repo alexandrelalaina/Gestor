b23fad9cb7695186eb53c1f7dcf4485a9e29373a
Alexandre La Laina <alexandre_lalaina@yahoo.com.br>
2018-11-22 01:11:42 -0200

    181122 Configurando

--- Gestor/settings.py
+++ Gestor/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://gestor-lalaina.herokuapp.com']
+ALLOWED_HOSTS = ['gestor-lalaina.herokuapp.com']
 
 
 # Application definition
